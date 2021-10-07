55987d93d0dfbb24982e8377106c5277bd647539
jaakkoniemensivu <jaakko.niemensivu@hotmail.com>
2021-10-07 14:57:03 +0300

    Add base model

--- src/models/User.py
+++ src/models/User.py
@@ -1,1 +1,15 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner")
